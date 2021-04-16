26566983e5d8547e6ed9306e083b19ec6c8fec46
ser2020095 <70794177+ser2020095@users.noreply.github.com>
2021-04-16 19:16:43 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,10 +14,6 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.15.1/g' package/base-files/files/bin/config_generate
 
-# Add luci-app-diskman
-git clone --depth=1 https://github.com/lisaac/luci-app-diskman
-mkdir parted
-cp luci-app-diskman/Parted.Makefile parted/Makefile
 
 cat >> feeds/luci/modules/luci-base/po/zh_Hans/base.po <<EOF
 #: modules/luci-base/root/usr/share/luci/menu.d/luci-base.json:45
