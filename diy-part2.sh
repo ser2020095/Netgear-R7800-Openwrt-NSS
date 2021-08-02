0eafc86d3e20eb87c9f528f607f6d608875183da
ser2020095 <70794177+ser2020095@users.noreply.github.com>
2021-08-02 15:02:08 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,7 +15,7 @@
 sed -i 's/192.168.1.1/192.168.15.1/g' package/base-files/files/bin/config_generate
 
 
-cat >> feeds/luci/modules/luci-base/po/zh_Hans/base.po <<EOF
+# cat >> feeds/luci/modules/luci-base/po/zh_Hans/base.po <<EOF
 #: modules/luci-base/root/usr/share/luci/menu.d/luci-base.json:45
 msgid "NAS"
 msgstr "网络存储"
