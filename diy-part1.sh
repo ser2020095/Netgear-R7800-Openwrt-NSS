d26bd70ba336846f07c2cc7925be3e69c7d85565
ser2020095 <70794177+ser2020095@users.noreply.github.com>
2021-08-03 09:35:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,7 +18,7 @@ git clone https://github.com/chenhw2/openwrt-v2ray-plugin.git package/v2ray-plug
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 #git clone https://github.com/fw876/helloworld.git package/openwrt-helloworld
 #git clone https://github.com/MrH723/openwrt-packages.git package/openwrt-packages
-sed -i '$a src-git WikiHacker https://github.com/WikiHacker/openwrt-packages' feeds.conf.default
+#sed -i '$a src-git WikiHacker https://github.com/WikiHacker/openwrt-packages' feeds.conf.default
 # Add a feed source
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 #
